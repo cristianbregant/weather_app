cb1fca51eb9e9e15f73ffa136818092528f543ea
Cristian Bregant <cristian.bregant94@gmail.com>
2022-04-22 16:12:05 +0200

    Bugfix: - fixed status bar color in light and dark mode

--- lib/src/screen/location.dart
+++ lib/src/screen/location.dart
@@ -2,6 +2,7 @@ import 'package:alpian_weather_flutter/src/provider/app_config_provider.dart';
 import 'package:alpian_weather_flutter/src/provider/location_provider.dart';
 import 'package:alpian_weather_flutter/src/utils/utils.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 
 class Location extends StatefulWidget {
@@ -13,6 +14,7 @@ class Location extends StatefulWidget {
 
 class _LocationState extends State<Location> {
   late TextEditingController _controller;
+  bool get isDarkMode => Theme.of(context).brightness == Brightness.dark;
 
   void _confirmPressed() {
     context.read<LocationProvider>().setLocation(_controller.text);
@@ -34,52 +36,56 @@ class _LocationState extends State<Location> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: [
-            Row(
-              children: const [
-                Expanded(
-                  child: Padding(
-                    padding: EdgeInsets.fromLTRB(8.0, 16, 16, 16),
-                    child: Text(
-                      "Location",
-                      style:
-                          TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value:
+          isDarkMode ? SystemUiOverlayStyle.light : SystemUiOverlayStyle.dark,
+      child: Scaffold(
+        body: SafeArea(
+          child: Column(
+            children: [
+              Row(
+                children: const [
+                  Expanded(
+                    child: Padding(
+                      padding: EdgeInsets.fromLTRB(8.0, 16, 16, 16),
+                      child: Text(
+                        "Location",
+                        style: TextStyle(
+                            fontSize: 32, fontWeight: FontWeight.bold),
+                      ),
                     ),
                   ),
-                ),
-              ],
-            ),
-            Padding(
-                padding: const EdgeInsets.all(16),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text(
-                        "Choose the city for which you want to know the weather forecast",
-                        style: TextStyle(fontSize: 18)),
-                    const SizedBox(
-                      height: 8,
-                    ),
-                    TextFormField(
-                      controller: _controller,
-                    ),
-                    const SizedBox(
-                      height: 16,
-                    ),
-                    Row(
-                      children: [
-                        Expanded(
-                            child: ElevatedButton(
-                                onPressed: _confirmPressed,
-                                child: const Text("Confirm"))),
-                      ],
-                    )
-                  ],
-                ))
-          ],
+                ],
+              ),
+              Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                          "Choose the city for which you want to know the weather forecast",
+                          style: TextStyle(fontSize: 18)),
+                      const SizedBox(
+                        height: 8,
+                      ),
+                      TextFormField(
+                        controller: _controller,
+                      ),
+                      const SizedBox(
+                        height: 16,
+                      ),
+                      Row(
+                        children: [
+                          Expanded(
+                              child: ElevatedButton(
+                                  onPressed: _confirmPressed,
+                                  child: const Text("Confirm"))),
+                        ],
+                      )
+                    ],
+                  ))
+            ],
+          ),
         ),
       ),
     );
